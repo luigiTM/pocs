57a6e66099454a36f0846926e2fd5ac3a0b04b66
Luigi Tosin Misturini <luigitosin@gmail.com>
2026-04-29 20:47:58 -0300

    Starting the functional interfaces poc

---
+++ java/functional-interfaces/src/test/java/com/lughtech/functionaInterfaces/function/FunctionTest.java
@@ -0,0 +1,65 @@
+package com.lughtech.functionaInterfaces.function;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FunctionTest {
+
+    @Test
+    void givenText_whenReturnFirstLetter_thenReturnT() {
+        Function<String, Character> returnFirstLetter = s -> s.charAt(0);
+
+        assertEquals('T', returnFirstLetter.apply("Text"));
+    }
+
+    void givenOne_whenConvertIntToString_thenReturnStringOne() {
+        IntFunction<String> convertIntToString = String::valueOf;
+
+        assertEquals("1", convertIntToString.apply(1));
+    }
+
+    void givenOne_whenConvertLongToString_thenReturnStringOne() {
+        LongFunction<String> convertLongToString = String::valueOf;
+
+        assertEquals("1", convertLongToString.apply(1L));
+    }
+
+    void givenOne_whenConvertDoubleToString_thenReturnStringOne() {
+        DoubleFunction<String> convertDoubleToString = String::valueOf;
+
+        assertEquals("1", convertDoubleToString.apply(1D));
+    }
+
+    void givenStringOne_whenConvertStringToInt_thenReturnOne() {
+        ToIntFunction<String> convertStringToIn = Integer::valueOf;
+
+        assertEquals(1, convertStringToIn.applyAsInt("1"));
+    }
+
+    void givenStringOne_whenConvertStringToLong_thenReturnOne() {
+        ToLongFunction<String> convertStringToLong = Long::valueOf;
+
+        assertEquals(1L, convertStringToLong.applyAsLong("1"));
+    }
+
+    void givenStringOne_whenConvertStringToDouble_thenReturnOne() {
+        ToDoubleFunction<String> convertStringToDouble = Double::valueOf;
+
+        assertEquals(1D, convertStringToDouble.applyAsDouble("1"));
+    }
+
+    // There are also DoubleToIntFunction, DoubleToLongFunction, IntToDoubleFunction,
+    // IntToLongFunction, LongToIntFunction, LongToDoubleFunction
+
+    @Test
+    void givenText_whenReturnConcatenatedText_thenReturnConcatenated() {
+        BiFunction<String, String, String> concatenateText = (s1, s2) -> s1 + " " +s2;
+
+        assertEquals("Concatenated Text", concatenateText.apply("Concatenated", "Text"));
+    }
+
+    // There are also ToDoubleBiFunction, ToIntBiFunction, and ToLongBiFunction to return primitive types.
+}
