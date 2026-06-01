05ea8381d2b29a7c0077a639666e02302ac349ad
Luigi Tosin Misturini <luigitosin@gmail.com>
2026-06-01 07:06:43 -0300

    Finishing joda time basics

--- java/java-time-and-joda-time/src/test/java/com/lughtech/jodatime/BasicsTest.java
+++ java/java-time-and-joda-time/src/test/java/com/lughtech/jodatime/BasicsTest.java
@@ -1,6 +1,7 @@
 package com.lughtech.jodatime;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDate;
 import org.junit.jupiter.api.Test;
 
@@ -42,7 +43,42 @@ public class BasicsTest {
         assertEquals(1, firstOfJanuaryTwoThousandMidday.getMonthOfYear());
         assertEquals(1, firstOfJanuaryTwoThousandMidday.getDayOfMonth());
         assertEquals(12, firstOfJanuaryTwoThousandMidday.getHourOfDay());
-        assertEquals(0, firstOfJanuaryTwoThousandMidday.getMinuteOfDay());
-        assertEquals(0, firstOfJanuaryTwoThousandMidday.getSecondOfDay());
+        assertEquals(0, firstOfJanuaryTwoThousandMidday.getMinuteOfHour());
+        assertEquals(0, firstOfJanuaryTwoThousandMidday.getSecondOfMinute());
+    }
+
+    @Test
+    void shouldCreateDateTimeWithDefaultTimezone() {
+        DateTimeZone defaultDateTimeZone = DateTimeZone.forTimeZone(null);
+        System.out.println(defaultDateTimeZone);
+
+        assertNotNull(defaultDateTimeZone);
+    }
+
+    @Test
+    void shouldCreateDateTimeZoneUTC() {
+        DateTimeZone dateTimeZoneUTC = DateTimeZone.UTC;
+
+        assertEquals("UTC", dateTimeZoneUTC.getID());
+    }
+
+    @Test
+    void shouldCreateNewLocalDateWithoutChangingOrigin() {
+        LocalDate firstOfJanuaryTwoThousand = LocalDate.parse("2000-01-01");
+        LocalDate secondOfJanuaryTwoThousand = firstOfJanuaryTwoThousand.plusDays(1);
+
+        assertEquals(1, firstOfJanuaryTwoThousand.getDayOfMonth());
+        assertEquals(2, secondOfJanuaryTwoThousand.getDayOfMonth());
+        assertEquals(secondOfJanuaryTwoThousand.getDayOfMonth(), firstOfJanuaryTwoThousand.getDayOfMonth() + 1);
+    }
+
+    @Test
+    void shouldCreateNewLocalDateTimeWithoutChangingOrigin() {
+        DateTime firstOfJanuaryTwoThousandMidday = DateTime.parse("2000-01-01T12:00:00");
+        DateTime firstOfJanuaryTwoThousandOneHour = firstOfJanuaryTwoThousandMidday.plusHours(1);
+
+        assertEquals(12, firstOfJanuaryTwoThousandMidday.getHourOfDay());
+        assertEquals(13, firstOfJanuaryTwoThousandOneHour.getHourOfDay());
+        assertEquals(firstOfJanuaryTwoThousandOneHour.getHourOfDay(), firstOfJanuaryTwoThousandMidday.getHourOfDay() + 1);
     }
 }
